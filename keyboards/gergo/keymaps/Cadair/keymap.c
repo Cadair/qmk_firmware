998049630eb4a191de8dfd9c2885c930f153b418
Stuart Mumford <stuart@cadair.com>
2021-02-15 10:39:34 +0000

    Tweaks to gaming

--- keyboards/gergo/keymaps/Cadair/keymap.c
+++ keyboards/gergo/keymaps/Cadair/keymap.c
@@ -152,11 +152,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------'       `--------------'
  */
 [GAME] = LAYOUT_gergo(
-    KC_F5,   KC_Q,  KC_W,   KC_E,  KC_R, KC_T,                                                   KC_Y,  KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
-    KC_LSFT,  KC_A,  KC_S,   KC_D,  KC_F, KC_G, KC_F1,                      LM(NUMB, MOD_LGUI),  KC_H,  KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    MOD_LCTL, KC_Z,  KC_X,   KC_C,  KC_V, KC_B, KC_F2,  KC_F3,  KC_PGDN, KC_BSPC,             KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+    KC_F5,   KC_Q,  KC_W,   KC_E,  KC_R, KC_T,                                                          KC_Y,  KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
+    KC_LSFT,  KC_A,  KC_S,   KC_D,  KC_F, KC_G, KC_F1,                             LM(NUMB, MOD_LGUI),  KC_H,  KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LCTL, KC_Z,  KC_X,   KC_C,  KC_V, KC_B, KC_F2,  KC_F3,     KC_ESC,        KC_BSPC,             KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
 
-    MOD_LALT,  KC_TAB, KC_SPC, LT(KC_ESC, KC_ENT),              LT(SYMB, KC_ENT), LT(NUMB, KC_SPC),  KC_TAB, KC_RALT
+    KC_LALT,  KC_TAB, KC_SPC, KC_ENT,                             LT(SYMB, KC_ENT), LT(NUMB, KC_SPC),  KC_TAB, KC_RALT
     ),  
 };
 /* Keymap template
